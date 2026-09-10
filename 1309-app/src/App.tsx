2af1b9948f7697ef4d75b465936daa21158182e2
Ricardo Vieira <ricardovieiracdo@gmail.com>
2026-09-10 12:44:20 -0300

    Restaura a chuva de pétalas
    Recoloca o componente Petals, o CSS de queda das pétalas e o
    render em App, removidos por engano no commit "adding photos".

--- 1309-app/src/App.tsx
+++ 1309-app/src/App.tsx
@@ -1,10 +1,12 @@
 import { Title } from './components/Title'
 import { Photo } from './components/Photo'
 import { Counter } from './components/Counter'
+import { Petals } from './components/Petals'
 
 export function App() {
   return (
     <>
+      <Petals />
       <Title />
       <Photo />
       <Counter />
